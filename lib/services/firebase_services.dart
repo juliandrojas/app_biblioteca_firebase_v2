2d3344cc6068fdf06013073ff6335e7c236ce24f
Julián Rojas <juliandra140201@gmail.com>
2023-04-17 07:33:40 -0500

    Add the futureList obtenerLibros

---
+++ lib/services/firebase_services.dart
@@ -0,0 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+//Instance of FirebaseFirestore
+FirebaseFirestore db = FirebaseFirestore.instance;
+
+//Función para traer los datos (consulta a la BD)
+Future<List> obtenerLibros() async {
+  List libros = [];
+  //Referencia a la colección (La colección se llama libros)
+  CollectionReference collectionReferenceLibros = db.collection('libros');
+  //Nos traemos la información de la colección
+  QuerySnapshot queryLibros = await collectionReferenceLibros.get();
+  //Recorremos la lista de documentos
+  queryLibros.docs.forEach((documento) {
+    //Agregamos los documentos a la lista
+    libros.add(documento.data());
+  });
+  return libros;
+}
