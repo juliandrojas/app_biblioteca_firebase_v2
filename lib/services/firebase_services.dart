961584c35054eb37472460e2a892bd3aac1d3ab5
Julián Rojas <juliandra140201@gmail.com>
2023-04-17 09:46:49 -0500

    Add the method obtenerEstudiantes

--- lib/services/firebase_services.dart
+++ lib/services/firebase_services.dart
@@ -3,17 +3,29 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 //Instance of FirebaseFirestore
 FirebaseFirestore db = FirebaseFirestore.instance;
 
-//Función para traer los datos (consulta a la BD)
-Future<List> getBooks() async {
+//Función para traer los datos de los libros (consulta a la BD)
+Future<List> obtenerLibros() async {
+  //Lista que vamos a regresar
   List libros = [];
-  //Referencia a la colección (La colección se llama libros)
   CollectionReference collectionReferenceLibros = db.collection('libros');
-  //Nos traemos la información de la colección
   QuerySnapshot queryLibros = await collectionReferenceLibros.get();
-  //Recorremos la lista de documentos
+  //Iteramos los documentos
   queryLibros.docs.forEach((documento) {
-    //Agregamos los documentos a la lista
     libros.add(documento.data());
   });
   return libros;
+}
+
+//Función para traer los datos de los estudiantes (consulta a la BD)
+Future<List> obtenerEstudiantes() async {
+  //Lista que vamos a regresar
+  List estudiantes = [];
+  CollectionReference collectionReferenceEstudiantes =
+      db.collection('estudiantes');
+  QuerySnapshot queryEstudiantes = await collectionReferenceEstudiantes.get();
+  //Iteramos los documentos
+  queryEstudiantes.docs.forEach((documento) {
+    estudiantes.add(documento.data());
+  });
+  return estudiantes;
 }
