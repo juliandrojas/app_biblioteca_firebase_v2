85cf1ea3c84d96ef044bf687172cd50793066c85
Julián Rojas <juliandra140201@gmail.com>
2023-04-17 16:31:39 -0500

    Add the page add_student

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 //Pages
 import 'package:app_biblioteca_firebase_v2/pages/home.dart';
+import 'package:app_biblioteca_firebase_v2/pages/add_student.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,12 +20,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Material App Bar"),
-        ),
-        body: const Home(),
-      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const Home(),
+        '/addStudent': (context) => const AddStudent(),
+      },
     );
   }
 }
