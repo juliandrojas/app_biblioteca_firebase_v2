81b97cd7f5417676919582e91fab8305f4f3c523
Julián Rojas <juliandra140201@gmail.com>
2023-04-17 16:34:06 -0500

    Files and more files :0

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -12,6 +12,9 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+      ),
       body: FutureBuilder(
         future: obtenerLibros(),
         builder: (context, snapshot) {
