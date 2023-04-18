e157d1e43504d978f412880602625430c23c6dec
Julián Rojas <juliandra140201@gmail.com>
2023-04-17 22:09:55 -0500

    Create the structure of DevolverLibro class

---
+++ lib/pages/devolver_libro.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DevolverLibro extends StatefulWidget {
+  const DevolverLibro({super.key});
+
+  @override
+  State<DevolverLibro> createState() => _DevolverLibroState();
+}
+
+class _DevolverLibroState extends State<DevolverLibro> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
