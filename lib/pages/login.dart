3b1a46b885a4f69dec544431cc021fb1d7bb475a
Julián Rojas <juliandra140201@gmail.com>
2023-04-18 17:17:10 -0500

    Fix the card of login.dart file

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -16,40 +16,37 @@ class _LoginState extends State<Login> {
       ),
       body: Center(
         child: Card(
-          child: Container(
+          child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                TextFormField(
-                  decoration: const InputDecoration(
-                    labelText: 'Correo electrónico',
-                  ),
+            child: Column(mainAxisSize: MainAxisSize.min, children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Correo electrónico',
                 ),
-                const SizedBox(height: 16.0),
-                TextFormField(
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                    labelText: 'Contraseña',
-                  ),
+              ),
+              const SizedBox(height: 16.0),
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                  labelText: 'Contraseña',
                 ),
-                const SizedBox(height: 16.0),
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, '/home');
-                  },
-                  child: const Text('Iniciar sesión'),
-                ),
-                const SizedBox(height: 16.0),
-                ElevatedButton(
-                  onPressed: () async {
-                    await Navigator.pushNamed(context, '/addStudent');
-                    setState(() {});
-                  },
-                  child: const Text('Registrarse'),
-                ),
-              ],
-            ),
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/home');
+                },
+                child: const Text('Iniciar sesión'),
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () async {
+                  await Navigator.pushNamed(context, '/addStudent');
+                  setState(() {});
+                },
+                child: const Text('Registrarse'),
+              ),
+            ]),
           ),
         ),
       ),
