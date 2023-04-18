bb860e8f11981d48950322fa1a8e7b5ebf69a18b
Julián Rojas <juliandra140201@gmail.com>
2023-04-17 22:10:34 -0500

    Create the ListaLibrosDisponibles class

---
+++ lib/pages/lista_libros_disponibles.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:app_biblioteca_firebase_v2/services/firebase_services.dart';
+
+class ListaLibrosDisponibles extends StatefulWidget {
+  const ListaLibrosDisponibles({super.key});
+
+  @override
+  State<ListaLibrosDisponibles> createState() => _ListaLibrosDisponiblesState();
+}
+
+class _ListaLibrosDisponiblesState extends State<ListaLibrosDisponibles> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Lista de Libros Disponibles"),
+      ),
+      body: FutureBuilder(
+        future: obtenerLibros(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data?.length,
+              itemBuilder: ((context, index) {
+                return ListTile(
+                  title: Text(snapshot.data?[index]['titulo']),
+                  subtitle: Text(snapshot.data?[index]['autor']),
+                  trailing: ElevatedButton(
+                      onPressed: () {
+                        print(snapshot.data?[index]['titulo']);
+                      },
+                      child: const Text("Reservar")),
+                );
+              }),
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
