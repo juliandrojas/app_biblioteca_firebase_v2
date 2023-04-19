fd401ba7e09d235ece569528dd4f5ff3332cec1b
Julián Rojas <juliandra140201@gmail.com>
2023-04-18 22:28:15 -0500

    Controllers and method to check if a student is register

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,3 +1,4 @@
+import 'package:app_biblioteca_firebase_v2/services/firebase_services.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -8,6 +9,8 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  TextEditingController correoController = TextEditingController(text: "");
+  TextEditingController contrasenaController = TextEditingController(text: "");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,28 +23,77 @@ class _LoginState extends State<Login> {
             padding: const EdgeInsets.all(16.0),
             child: Column(mainAxisSize: MainAxisSize.min, children: [
               const Text(
-                "Ingreso",
+                "INGRESO A LA APP",
                 style: TextStyle(
                   fontSize: 30,
                   fontWeight: FontWeight.bold,
                 ),
               ),
               TextFormField(
+                controller: correoController,
                 decoration: const InputDecoration(
                   labelText: 'Correo electrónico',
+                  hintText: 'Digita tu correo electrónico',
                 ),
               ),
               const SizedBox(height: 16.0),
               TextFormField(
+                controller: contrasenaController,
                 obscureText: true,
                 decoration: const InputDecoration(
                   labelText: 'Contraseña',
+                  hintText: 'Digita tu contraseña',
                 ),
               ),
               const SizedBox(height: 16.0),
               ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/home');
+                onPressed: () async {
+                  if (correoController.text.isNotEmpty &&
+                      contrasenaController.text.isNotEmpty) {
+                    bool verificarEstudianteRegistrado =
+                        await verificarEstudiante(
+                            correoController.text, contrasenaController.text);
+                    //Si el estudiante está registrado, procedemos a ir a home
+                    if (verificarEstudianteRegistrado) {
+                      Navigator.pushNamed(context, '/home');
+                    } else {
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: const Text("Error"),
+                              content: const Text(
+                                  'Regístrate para acceder a la app'),
+                              actions: [
+                                TextButton(
+                                  child: const Text("Cerrar"),
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                ),
+                              ],
+                            );
+                          });
+                    }
+                  } else {
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: const Text("Error"),
+                            content:
+                                const Text('Ingrese el correo y la contraseña'),
+                            actions: [
+                              TextButton(
+                                child: const Text("Cerrar"),
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                              ),
+                            ],
+                          );
+                        });
+                  }
                 },
                 child: const Text('Iniciar sesión'),
               ),
