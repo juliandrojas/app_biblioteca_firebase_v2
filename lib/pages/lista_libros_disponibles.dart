071415d475be4c7167908266060a9b9e1da1504a
Julián Rojas <juliandra140201@gmail.com>
2023-04-25 15:58:27 -0500

    Add the const Text "Reservar" and refactoring with aditional info

--- lib/pages/lista_libros_disponibles.dart
+++ lib/pages/lista_libros_disponibles.dart
@@ -25,18 +25,43 @@ class _ListaLibrosDisponiblesState extends State<ListaLibrosDisponibles> {
                 return ListTile(
                   title: Text(snapshot.data?[index]['titulo']),
                   subtitle: Text(snapshot.data?[index]['autor']),
-                  trailing: ElevatedButton(
-                      onPressed: () async {
-                        //print(snapshot.data?[index]['titulo']);
-                        await Navigator.pushNamed(
-                            context, "/formulario_prestamo",
-                            arguments: {
-                              "poseedor": snapshot.data?[index]['poseedor'],
-                              "uid": snapshot.data?[index]['uid'],
-                            });
-                        setState(() {});
-                      },
-                      child: const Text("Reservar")),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () async {
+                          await Navigator.pushNamed(
+                              context, "/formulario_prestamo",
+                              arguments: {
+                                "poseedor": snapshot.data?[index]['poseedor'],
+                                "uid": snapshot.data?[index]['uid'],
+                              });
+                          setState(() {});
+                        },
+                        child: const Text("Reservar"),
+                      ),
+                      PopupMenuButton(
+                        itemBuilder: (BuildContext context) {
+                          return [
+                            PopupMenuItem(
+                              child: ListTile(
+                                title: Text(snapshot.data?[index]['titulo']),
+                                subtitle: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(snapshot.data?[index]['categoria']),
+                                    Text(snapshot.data?[index]['descripcion']),
+                                    Text(snapshot.data?[index]['editorial']),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ];
+                        },
+                        icon: Icon(Icons.info),
+                      ),
+                    ],
+                  ),
                 );
               }),
             );
