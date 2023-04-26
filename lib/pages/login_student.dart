e96757614889129b19aa2196e5a33ef2a73c9f61
Julián Rojas <juliandra140201@gmail.com>
2023-04-25 22:10:21 -0500

    Add the class LoginStudent

---
+++ lib/pages/login_student.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LoginStudent extends StatefulWidget {
+  const LoginStudent({super.key});
+
+  @override
+  State<LoginStudent> createState() => _LoginStudentState();
+}
+
+class _LoginStudentState extends State<LoginStudent> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
