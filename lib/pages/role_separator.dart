55f7fd9082fe00dfc06b11784b1088628678b403
Julián Rojas <juliandra140201@gmail.com>
2023-04-25 22:09:31 -0500

    Add the class RoleSeparator to separate admin and student roles

--- lib/pages/role_separator.dart
+++ lib/pages/role_separator.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+
+const Color greenUTS = Color.fromRGBO(203, 212, 36, 1);
+
 class RoleSeparator extends StatefulWidget {
   const RoleSeparator({super.key});
 
@@ -9,6 +12,43 @@ class RoleSeparator extends StatefulWidget {
 class _RoleSeparatorState extends State<RoleSeparator> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Role Separator"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/logoApp.png'),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(200, 50), // Ajusta el tamaño aquí
+                ),
+                onPressed: () {
+                  // Acción al presionar "Ingreso Administrador"
+                  Navigator.pushNamed(context, '/login_admin');
+                },
+                child: const Text('Ingreso Administrador'),
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(200, 50), // Ajusta el tamaño aquí
+                ),
+                onPressed: () {
+                  // Acción al presionar "Devolver Libro"
+                  Navigator.pushNamed(context, '/login_student');
+                },
+                child: const Text('Ingreso Estudiantes'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
