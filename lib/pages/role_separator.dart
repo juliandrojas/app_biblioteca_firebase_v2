be5ae13df93e15f0704bd4b8acb813a6426f77f8
Julián Rojas <juliandra140201@gmail.com>
2023-04-25 21:47:50 -0500

    Create a roleSeparator class

---
+++ lib/pages/role_separator.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class RoleSeparator extends StatefulWidget {
+  const RoleSeparator({super.key});
+
+  @override
+  State<RoleSeparator> createState() => _RoleSeparatorState();
+}
+
+class _RoleSeparatorState extends State<RoleSeparator> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
